5115d8aec4e3fd53fdf47c7451bc245b88c4282b
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-18 14:50:32 +0900

    feat: 배포환경 라우팅 설정

--- src/main/java/com/eventorgateway/config/RouteLocatorConfig.java
+++ src/main/java/com/eventorgateway/config/RouteLocatorConfig.java
@@ -4,6 +4,7 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import com.eventorgateway.auth.filter.AuthorizationHeaderFilter;
 
@@ -14,8 +15,9 @@ import lombok.RequiredArgsConstructor;
 public class RouteLocatorConfig {
 	private final AuthorizationHeaderFilter authorizationHeaderFilter;
 
+	@Profile("dev")
 	@Bean
-	public RouteLocator prodRouteLocator(RouteLocatorBuilder builder) {
+	public RouteLocator devRouteLocator(RouteLocatorBuilder builder) {
 		return builder.routes()
 			// .route("bookstore-back", r -> r.path("/back/**")
 			//         .uri("lb://book-store-back") // 로드밸런싱 활성화
@@ -29,6 +31,23 @@ public class RouteLocatorConfig {
 			)
 			.route("eventor-auth", r -> r.path("/auth/**", "/oauth2/**")
 				.uri("http://localhost:8070")
+			)
+			.build();
+	}
+
+	@Profile("prod")
+	@Bean
+	public RouteLocator prodRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+			.route("eventor-back", r -> r.path("/back/users/signup/**", "/back/users/recover/**")
+				.uri("http://eventor-back:8083") // 컨테이너 이름 사용
+			)
+			.route("eventor-back", r -> r.path("/back/**")
+				.filters(f -> f.filter(authorizationHeaderFilter.apply(new AuthorizationHeaderFilter.Config())))
+				.uri("http://eventor-back:8083") // 컨테이너 이름 사용
+			)
+			.route("eventor-auth", r -> r.path("/auth/**", "/oauth2/**")
+				.uri("http://eventor-auth:8070") // 컨테이너 이름 사용
 			)
 			.build();
 	}
