31047c4c7589e9ba1cb8d6dcfc6b55dd21c1a1bc
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-19 17:54:55 +0900

    feat: 유레카 서비스 등록

--- src/main/java/com/eventorgateway/EventorGatewayApplication.java
+++ src/main/java/com/eventorgateway/EventorGatewayApplication.java
@@ -2,7 +2,9 @@ package com.eventorgateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class EventorGatewayApplication {
 
