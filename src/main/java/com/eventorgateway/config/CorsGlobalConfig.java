9d71c6d5906aa2d8e43538843ecfe4e8f3b7a466
dlrudgjs104 <92204410+dlrudgjs104@users.noreply.github.com>
2025-02-25 23:44:23 +0900

    feat: cors 설정 수정

--- src/main/java/com/eventorgateway/config/CorsGlobalConfig.java
+++ src/main/java/com/eventorgateway/config/CorsGlobalConfig.java
@@ -1,30 +1,30 @@
-// package com.eventorgateway.config;
-//
-// import org.springframework.beans.factory.annotation.Value;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import org.springframework.web.cors.CorsConfiguration;
-// import org.springframework.web.cors.reactive.CorsWebFilter;
-// import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-//
-// @Configuration
-// public class CorsGlobalConfig {
-//
-// 	@Value("${cors.allowed-origins}")
-// 	private String allowedOrigins;
-//
-//
-// 	@Bean
-// 	public CorsWebFilter corsWebFilter() {
-// 		CorsConfiguration corsConfig = new CorsConfiguration();
-// 		corsConfig.addAllowedOrigin(allowedOrigins);
-// 		corsConfig.addAllowedMethod("*");
-// 		corsConfig.addAllowedHeader("*");
-// 		corsConfig.setAllowCredentials(true);
-//
-// 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-// 		source.registerCorsConfiguration("/**", corsConfig);
-//
-// 		return new CorsWebFilter(source);
-// 	}
-// }
+package com.eventorgateway.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class CorsGlobalConfig {
+
+	@Value("${cors.allowed-origins}")
+	private String allowedOrigins;
+
+
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		CorsConfiguration corsConfig = new CorsConfiguration();
+		corsConfig.addAllowedOrigin(allowedOrigins);
+		corsConfig.addAllowedMethod("*");
+		corsConfig.addAllowedHeader("*");
+		corsConfig.setAllowCredentials(true);
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", corsConfig);
+
+		return new CorsWebFilter(source);
+	}
+}
